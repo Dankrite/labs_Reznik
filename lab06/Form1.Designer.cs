ba80f5cd616b4276c3dc2a1c54d2d29ab7c79fe1
Rznk_DV <rznkdv12@gmail.com>
2023-03-25 14:07:04 +0200

    Довга сума, використання масиву

---
+++ lab06/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace lab06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.sumNumberTextBox = new System.Windows.Forms.TextBox();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(47, 30);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(387, 20);
+            this.firstNumberTextBox.TabIndex = 0;
+            this.firstNumberTextBox.TextChanged += new System.EventHandler(this.firstNumberTextBox_TextChanged);
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(47, 72);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(387, 20);
+            this.secondNumberTextBox.TabIndex = 1;
+            // 
+            // sumNumberTextBox
+            // 
+            this.sumNumberTextBox.Location = new System.Drawing.Point(47, 176);
+            this.sumNumberTextBox.Name = "sumNumberTextBox";
+            this.sumNumberTextBox.Size = new System.Drawing.Size(387, 20);
+            this.sumNumberTextBox.TabIndex = 2;
+            // 
+            // plusButton
+            // 
+            this.plusButton.Location = new System.Drawing.Point(208, 123);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(75, 23);
+            this.plusButton.TabIndex = 3;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(47, 238);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Очистити";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(359, 238);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Вихід";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 270);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.sumNumberTextBox);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.TextBox sumNumberTextBox;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ lab06/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int maxLen = 1 + Math.Max(firstNumberTextBox.Text.Length, secondNumberTextBox.Text.Length);
+            int[] number1 = new int[maxLen];
+            int[] number2 = new int[maxLen];
+            int[] sum = new int[maxLen];
+            int[] extra = new int[maxLen];
+            int numberIdx, textIdx;
+            numberIdx = maxLen - 1;
+            textIdx = firstNumberTextBox.Text.Length - 1;
+            while (textIdx >= 0)
+            {
+                number1[numberIdx] = (firstNumberTextBox.Text[textIdx] - '0');
+                textIdx--;
+                numberIdx--;
+            }
+            numberIdx = maxLen - 1;
+            textIdx = secondNumberTextBox.Text.Length - 1;
+            while (textIdx >= 0)
+            {
+                number2[numberIdx] = (secondNumberTextBox.Text[textIdx] - '0');
+                textIdx--;
+                numberIdx--;
+            }
+            for (numberIdx = maxLen - 1; numberIdx > 0; numberIdx--)
+            {
+                sum[numberIdx] = (number1[numberIdx] + number2[numberIdx] + extra[numberIdx]) % 10;
+                extra[numberIdx - 1] = (number1[numberIdx] + number2[numberIdx] + extra[numberIdx]) % 10;
+                sum[0] = extra[0];
+                bool isNumber = false;
+                foreach (int digit in sum)
+                {
+                    if (digit > 0)
+                    {
+                        isNumber = true;
+                        sumNumberTextBox.Text += digit;
+                    }
+                    else
+                    {
+                        if (isNumber)
+                            sumNumberTextBox.Text += digit;
+                    }
+                }
+            }
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            foreach (System.Windows.Forms.TextBox textbox in Controls.OfType<System.Windows.Forms.TextBox>())
+                textbox.Text = "";
+        }
+
+        private void firstNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
